5c459638eb37453dcc73436c8ba353b6382c2a55
Drew MacNicholas <dmacnicholas@gmail.com>
2022-08-01 23:58:40 -0500

    Add supermarkets controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   get 'customers/:id', to: 'customers#show'
 
+  get 'supermarkets/:id', to: 'supermarkets#show'
+
 end
--- spec/features/supermarket/show_spec.rb
+++ spec/features/supermarket/show_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe 'the supermarket show page' do
     customer_item_1 = CustomerItem.create!(item_id: item_2.id, customer_id: customer_1.id)
     customer_item_1 = CustomerItem.create!(item_id: item_3.id, customer_id: customer_1.id)
 
-    visit "supermarket/#{supermarket_1.id}"
+    visit "supermarkets/#{supermarket_1.id}"
 
     expect(page).to have_content("Publix")
     expect(page).to have_link("All Items", href: "/supermarkets/#{supermarket_1.id}/items")
