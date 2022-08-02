5825d05472fbc19c75c8c8a24ea8ab62f4a6fad8
Drew MacNicholas <dmacnicholas@gmail.com>
2022-08-02 00:24:22 -0500

    Add unique item model test

--- spec/models/supermarket_spec.rb
+++ spec/models/supermarket_spec.rb
@@ -6,4 +6,20 @@ RSpec.describe Supermarket, type: :model do
     it { should have_many(:customer_items).through(:customers) }
     it { should have_many(:items).through(:customer_items) }
   end
+
+  it 'can display a list of unique items' do
+    supermarket_1 = Supermarket.create!(name: "Publix", location: "Nashville, TN")
+    supermarket_2 = Supermarket.create!(name: "Trades Joes", location: "Nashville, TN")
+    customer_1 = Customer.create!(name: "Drew", supermarket_id: supermarket_1.id)
+    item_1 = Item.create!(name: "Steak", price: 20)
+    item_2 = Item.create!(name: "Potatoes", price: 10)
+    item_3 = Item.create!(name: "Green Beans", price: 6)
+    item_4 = Item.create!(name: "Salad", price: 4)
+    customer_item_1 = CustomerItem.create(item_id: item_1.id, customer_id: customer_1.id)
+    customer_item_1 = CustomerItem.create(item_id: item_2.id, customer_id: customer_1.id)
+    customer_item_1 = CustomerItem.create(item_id: item_3.id, customer_id: customer_1.id)
+
+    expect(supermarket_1.unique_items).to eq([item_1, item_2, item_3])
+  end
+
 end
