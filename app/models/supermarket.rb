62edf80202e37876f0ff4f310ceeab7a5d76f570
Drew MacNicholas <dmacnicholas@gmail.com>
2022-08-02 00:26:00 -0500

    Add unique item model method

--- app/models/supermarket.rb
+++ app/models/supermarket.rb
@@ -2,4 +2,8 @@ class Supermarket < ApplicationRecord
   has_many :customers
   has_many :customer_items, through: :customers
   has_many :items, through: :customer_items
+
+  def unique_items
+    items.distinct
+  end
 end
