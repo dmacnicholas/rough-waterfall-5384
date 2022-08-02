207777300232956be1415eea9d061dd11d62d28f
Drew MacNicholas <dmacnicholas@gmail.com>
2022-08-02 00:20:02 -0500

    Add supermarket_items controller and view and methods

--- app/controllers/supermarkets_controller.rb
+++ app/controllers/supermarkets_controller.rb
@@ -2,8 +2,4 @@ class SupermarketsController < ApplicationController
     def show
         @supermarket = Supermarket.find(params[:id])
     end
-
-    def index
-
-    end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'customers/:id', to: 'customers#show'
 
   get 'supermarkets/:id', to: 'supermarkets#show'
-  get 'supermarkets/:id/items', to: 'supermarkets#index'
+  get 'supermarkets/:id/items', to: 'supermarket_items#index'
 
 end
